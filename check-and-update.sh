5298979d482c9d16753f9417c1b5ad267f3d52c7
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2020-11-23 00:47:45 +0100

    Add scheduled update check

---
+++ check-and-update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+SPEC_VERSION=`grep "%define veracrypt_version" veracrypt.spec | cut -d " " -f 3`
+REMOTE_VERSION=`git ls-remote --tags https://github.com/veracrypt/VeraCrypt.git | grep "refs/tags/VeraCrypt_1" | tail -n 1 | cut -d "_" -f 2`
+
+if [ "${SPEC_VERSION}" = "${REMOTE_VERSION}" ]
+then
+    echo "No new version : skipping update"
+    exit 0
+fi
+
+echo "New version {${REMOTE_VERSION}} : updating"
+sed -i "s/veracrypt_version ${SPEC_VERSION}/veracrypt_version ${REMOTE_VERSION}/" veracrypt.spec
